5050599f5ccadadbb6d7fbdcc880319657ea96a9
krquinton <nscrexgst.dotr@gmail.com>
2022-06-03 22:02:24 +0800

    initial commit

---
+++ data.js
@@ -0,0 +1,293 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pool",
+      "name": "Pool",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.012432207072308188,
+          "pitch": -0.2730350037490261,
+          "rotation": 0,
+          "target": "5-beach"
+        },
+        {
+          "yaw": 0.01620924837488502,
+          "pitch": 0.12352941087578984,
+          "rotation": 0,
+          "target": "3-pavilion-"
+        },
+        {
+          "yaw": -2.958209354152105,
+          "pitch": -0.25206663927584394,
+          "rotation": 0,
+          "target": "2-facade"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-villa",
+      "name": "Villa",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.3439194187678325,
+        "pitch": 0.35655592618022425,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3841962476600163,
+          "pitch": 0.5040327421057675,
+          "rotation": 0,
+          "target": "4-room"
+        },
+        {
+          "yaw": 1.3841962476600163,
+          "pitch": 0.5040327421057675,
+          "rotation": 0,
+          "target": "4-room"
+        },
+        {
+          "yaw": -0.08279037213512552,
+          "pitch": 0.3249656624061785,
+          "rotation": 0,
+          "target": "3-pavilion-"
+        },
+        {
+          "yaw": -0.0017025493557056848,
+          "pitch": 0.7443176576480575,
+          "rotation": 0,
+          "target": "0-pool"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-facade",
+      "name": "Facade",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.08055365778434798,
+        "pitch": 0.595505565164018,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.17399978191106058,
+          "pitch": 0.3891570180208461,
+          "rotation": 0,
+          "target": "0-pool"
+        },
+        {
+          "yaw": -0.4156317681053725,
+          "pitch": -0.03802286365623786,
+          "rotation": 0,
+          "target": "5-beach"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-pavilion-",
+      "name": "Pavilion ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6638313256477746,
+          "pitch": -0.06846181920035299,
+          "rotation": 0,
+          "target": "4-room"
+        },
+        {
+          "yaw": 0.0004935992382169019,
+          "pitch": -0.15091801996080356,
+          "rotation": 0,
+          "target": "5-beach"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-room",
+      "name": "Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6027292642013276,
+          "pitch": 0.0815723662568395,
+          "rotation": 0,
+          "target": "0-pool"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-beach",
+      "name": "Beach",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.5236685457897536,
+        "pitch": 0.6542514275691893,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.01193790481815249,
+          "pitch": 0.5288973118162801,
+          "rotation": 0,
+          "target": "3-pavilion-"
+        },
+        {
+          "yaw": -0.32009315637529134,
+          "pitch": 0.3691664663790135,
+          "rotation": 0,
+          "target": "1-villa"
+        },
+        {
+          "yaw": -0.047829708567178386,
+          "pitch": 0.30258441226102484,
+          "rotation": 0,
+          "target": "2-facade"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Isora Resort - Puerto Galera",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
